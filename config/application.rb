4c6cdea7b00d4ffaaab6d609db74c8fc0d76a8e1
fakestarbaby <fakestarbaby@gmail.com>
2016-02-15 22:11:42 +0900

    Oooops! :scream_cat:

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module SampleApiServer
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.generatorsdo|g|
+    config.generators do |g|
       g.helper false
       g.javascripts false
       g.stylesheets false
