61faa2dcc68c69813acfd49e68f009f17f52702c
fakestarbaby <fakestarbaby@gmail.com>
2016-02-25 08:17:32 +0900

    Implement get product api and spec

--- spec/requests/apis/api/v1/products_spec.rb
+++ spec/requests/apis/api/v1/products_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 describe API::V1::Products do
-  describe "GET /api/v1/products" do
-    let!(:product) { create(:product) }
+  let!(:product) { create(:product) }
 
+  describe "GET /api/v1/products" do
     it "return 200 with an array of all products" do
       is_expected.to eq 200
       expect(response.body).to include_json(product.to_json)
+    end
+  end
+
+  describe "GET /api/v1/products/:id" do
+    let(:id) { product.id }
+
+    it "return 200 with a product" do
+      is_expected.to eq 200
     end
   end
 end
