22c399665473aa913ff241ba6e3259cd0ec8f9fa
fakestarbaby <fakestarbaby@gmail.com>
2016-02-29 08:41:12 +0900

    Implement swagger ui for product apis

--- app/apis/api/root.rb
+++ app/apis/api/root.rb
@@ -4,5 +4,10 @@ module API
     format :json
 
     mount API::V1::Root
+
+    add_swagger_documentation(
+      api_version: 'v1',
+      hide_documentation_path: true
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   mount API::Root => '/'
+  mount GrapeSwaggerRails::Engine => '/docs'
 end
